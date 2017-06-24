4708581337d4581d1f4b172501f8ad12801b18b9
root <root@localhost.localdomain>
2017-06-24 17:42:50 +0530

    fdsa

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+var cors=require('cors');
+var methodOverride=require('method-override');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
